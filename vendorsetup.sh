524fe83b1e0593b132a01dd9691abd97aaa6ff6e
bablu048 <bablu.b048@gmail.com>
2016-12-17 17:18:10 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
+add_lunch_combo chroma_bacon-userdebug
 add_lunch_combo chroma_angler-userdebug
 add_lunch_combo chroma_bullhead-userdebug
-add_lunch_combo chroma_bacon-userdebug
 add_lunch_combo chroma_hammerhead-userdebug
 add_lunch_combo chroma_mako-userdebug
 add_lunch_combo chroma_shamu-user
