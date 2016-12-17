b266934835cc6e2dcbaf479d61ac8f6cdf616c24
bablu048 <bablu.b048@gmail.com>
2016-12-17 17:45:48 +0530

    Add files via upload

---
+++ products/chroma_bacon.mk
@@ -0,0 +1,20 @@
+# Inherit AOSP device configuration for bacon.
+$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/chroma/products/common.mk)
+
+
+# Setup device specific product configuration.
+PRODUCT_NAME := chroma_bacon
+PRODUCT_BRAND := oneplus
+PRODUCT_DEVICE := bacon
+PRODUCT_MODEL := A0001
+PRODUCT_MANUFACTURER := OnePlus
+#PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+#TARGET_CONTINUOUS_SPLASH_ENABLED := true
+
+
+# Copy hammerhead specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/chroma/prebuilt/bacon/system/media/bootanimation.zip:system/media/bootanimation.zip
